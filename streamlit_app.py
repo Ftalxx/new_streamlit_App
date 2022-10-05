6f42b90b9f289f247c96e3f313449f1ff950d2eb
Ftalxx <115039139+Ftalxx@users.noreply.github.com>
2022-10-05 07:37:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title(':chicken: My New Healthy Dinner')
 
-streamlit.header('Breakfat Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled & Free-Range Eggs')
+streamlit.header('Breakfat Menu :avocado:')
+streamlit.text('Omega 3 & Blueberry Oatmeal :soup:')
+streamlit.text('Kale, Spinach & Rocket Smoothie :fruit:')
+streamlit.text('Hard-Boiled & Free-Range Eggs :egg:')
