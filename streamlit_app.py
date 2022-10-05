e114dcb1e7d48e22bb7816302087bac6edfb2392
Ftalxx <115039139+Ftalxx@users.noreply.github.com>
2022-10-05 07:59:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 #Pick list to pick the fruit / ending chooses specific item
 
-streamlit.multiselect("Pick your fruits:", list(my_fruit_list.index),['strawberries'])
+streamlit.multiselect("Pick your fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #Display
 
